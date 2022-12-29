d3936b0f3fb752575c8837f8bd00348e157f09c8
ddonkoh <donkoh72@gmail.com>
2022-12-29 15:37:04 +0000

    files added for image input

---
+++ Teamworks_2/Teamworks_2.Android/ImageService.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Teamworks_2.Droid;
+using Teamworks_2.Models;
+using Xamarin.Essentials;
+
+[assembly: Xamarin.Forms.Dependency(typeof(ImageService))]
+namespace Teamworks_2.Droid
+{
+    public class ImageService : IImageService
+    {
+        public async Task<string> PickImageAsync()
+        {
+            if (MediaPicker.IsCaptureSupported)
+            {
+                var file = await MediaPicker.PickPhotoAsync();
+                return file?.FullPath;
+            }
+            else
+            {
+                // Capture is not supported
+                return null;
+            }
+        }
+    }
+}
+
+
+
---
+++ Teamworks_2/Teamworks_2/Models/IImageService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Teamworks_2.Models
+{
+    public interface IImageService
+    {
+        Task<string> PickImageAsync();
+    }
+}
